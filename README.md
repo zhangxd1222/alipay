d80b27c5367c55fbcd210d56dc53469a9a938fb1
zhangxd1222 <zhangxd1222@chaqianma.com>
2016-07-28 17:50:50 +0800

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # alipay
+依照 https://github.com/charleyw/cordova-plugin-alipay 修改
+
+谢谢作者!!!
---
+++ src/android/AliPayPlugin.java
@@ -0,0 +1,61 @@
+package wang.imchao.plugin.alipay;
+
+import android.text.TextUtils;
+
+import com.alipay.sdk.app.PayTask;
+
+import org.apache.cordova.CallbackContext;
+import org.apache.cordova.CordovaInterface;
+import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.CordovaWebView;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+
+public class AliPayPlugin extends CordovaPlugin {
+    @Override
+    public void initialize(CordovaInterface cordova, CordovaWebView webView) {
+        super.initialize(cordova, webView);
+    }
+
+    @Override
+    public boolean execute(String action, JSONArray args, CallbackContext callbackContext) {
+        try {
+            JSONObject arguments = args.getJSONObject(0);
+            String orderInfo = arguments.getString("orderInfo");
+            this.pay(orderInfo, callbackContext);
+        } catch (JSONException e) {
+            callbackContext.error(new JSONObject());
+            e.printStackTrace();
+            return false;
+        }
+        return true;
+    }
+
+    public void pay(final String orderInfo, final CallbackContext callbackContext) {
+        cordova.getThreadPool().execute(new Runnable() {
+            @Override
+            public void run() {
+                // 构造PayTask 对象
+                PayTask alipay = new PayTask(cordova.getActivity());
+                // 调用支付接口，获取支付结果
+                String result = alipay.pay(orderInfo);
+                PayResult payResult = new PayResult(result);
+                if (TextUtils.equals(payResult.getResultStatus(), "9000")) {
+                    callbackContext.success(payResult.toJson());
+                } else {
+                    // 判断resultStatus 为非“9000”则代表可能支付失败
+                    // “8000”代表支付结果因为支付渠道原因或者系统原因还在等待支付结果确认，最终交易是否成功以服务端异步通知为准（小概率状态）
+                    if (TextUtils.equals(payResult.getResultStatus(), "8000")) {
+                        callbackContext.success(payResult.toJson());
+                    } else {
+                        callbackContext.error(payResult.toJson());
+                    }
+                }
+            }
+        });
+    }
+}
